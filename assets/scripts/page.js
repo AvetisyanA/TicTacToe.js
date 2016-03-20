2c9a9880fc57dc8788bb6e16f1181ce40954182b
Artyom Avetisyan <artyom.avatisyan@gmail.com>
2016-03-20 10:54:39 +0100

    popup fixes

--- assets/scripts/page.js
+++ assets/scripts/page.js
@@ -7,7 +7,7 @@
   var game = new TicTacToe();
 
   document.addEventListener("DOMContentLoaded", function(event) {
-    var newGameButton = document.getElementById("newGame");
+    var newGameButton = document.getElementById("new-game-button");
     var gameBoard = document.getElementsByClassName("game-board")[0];
     newGameButton.addEventListener("click", onNewGame);
     gameBoard.addEventListener("click", onGameEvent);
@@ -19,9 +19,9 @@
   }
 
   function onGameEvent(eventArgument) {
-    var gameBoard = document.getElementsByClassName("game-board")[0];
-    for (var i = 0; i < gameBoard.children.length; i++) {
-      var rowDiv = gameBoard.children[i];
+    var gameRows = document.getElementsByClassName("game-row");
+    for (var i = 0; i < gameRows.length; i++) {
+      var rowDiv = gameRows[i];
       for (var j = 0; j < rowDiv.children.length; j++) {
         if (eventArgument.target === rowDiv.children[j]) {
           if (!game.putX(i, j)) {
@@ -62,19 +62,19 @@
   }
 
   function updateBoard() {
-    var gameBoard = document.getElementsByClassName("game-board")[0];
+    var gameRows = document.getElementsByClassName("game-row");
     for (var i = 0; i < game.getBoardSize(); i++) {
       for (var j = 0; j < game.getBoardSize(); j++) {
           if (game.isX(i, j)) {
-            gameBoard.children[i].children[j].children[0].className =
+            gameRows[i].children[j].children[0].className =
               "fa fa-times fa-3x align";
           }
           else if(game.isO(i, j)) {
-            gameBoard.children[i].children[j].children[0].className =
+            gameRows[i].children[j].children[0].className =
               "fa fa-circle-o fa-3x align";
           }
           else {
-            gameBoard.children[i].children[j].children[0].className = "";
+            gameRows[i].children[j].children[0].className = "";
           }
       }
     }
